f213b40761e56b14d7f45792fd0a7d71ca4c5042
nwnp1 <stotoyaa@naver.com>
2022-01-27 04:28:21 +0900

    k_CT
    k번째 수

---
+++ k.js
@@ -0,0 +1,16 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    for (var i=0; i<commands.length;i++) {
+        var list = array.slice(commands[i][0]-1,commands[i][1]);
+        
+        list.sort((a, b) => {
+        return a-b;
+        });
+        
+        answer.push(list[commands[i][2]-1]);
+
+    };
+    
+    return answer;
+}
