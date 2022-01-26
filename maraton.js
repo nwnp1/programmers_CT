6ebbe1055d4c6c4323eecd0db9bed4c951221779
nwnp1 <stotoyaa@naver.com>
2022-01-26 17:17:37 +0900

    maraton_CT

---
+++ maraton.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+    participant.sort(); //문자열 정렬, reverse는 내림차순 정렬
+    completion.sort();
+    
+    for (var i=0; i<participant.length; i++) {
+        if ( participant[i] !== completion[i]) { //인덱스 0부터 순차적으로 두 배열 비교
+            return participant[i]; //비완주자가 참가자 배열에 나올 경우 출력
+				}
+    }
+}
