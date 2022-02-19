b19f45674625fac2230fbe66f0c203ec91b6fc7d
nwnp1 <stotoyaa@naver.com>
2022-02-19 11:47:22 +0900

    Create twoNumber.js

---
+++ twoNumber.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a<=b){
+        for(var i=a; i<=b; i++){
+            answer += i;
+        }
+    }else{
+        for(var i=b; i<=a; i++){
+            answer += i;
+        }
+    }
+    return answer;
+}
